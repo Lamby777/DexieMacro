e8de6a58bd53f3e35fcd08c741b795b8367e89db
Lamby777 <sheepareawesome777@gmail.com>
2019-12-06 23:37:10 -0500

    getting ready for app

--- macro.py
+++ macro.py
@@ -1,19 +1,18 @@
+from turtle import *
 try:
-    keyboard.press_and_release('shift+s, space')
-    keyboard.write('The quick brown fox jumps over the lazy dog.')
-    keyboard.add_hotkey('ctrl+shift+a', print, args=('triggered', 'hotkey'))
+	keyboard.press_and_release('shift+s, space')
+	keyboard.write('The quick brown fox jumps over the lazy dog.')
+	keyboard.add_hotkey('ctrl+shift+a', print, args=('triggered', 'hotkey'))
 
-    # Press PAGE UP then PAGE DOWN to type "foobar".
-    keyboard.add_hotkey('page up, page down', lambda: keyboard.write('foobar'))
+	# Press PAGE UP then PAGE DOWN to type "foobar".
+	keyboard.add_hotkey('page up', lambda: keyboard.write('foobar'))
 
-    # Blocks until you press esc.
-    keyboard.wait('esc')
+	# Blocks until you press esc.
+	keyboard.wait('esc')
 
-    # Record events until 'esc' is pressed.
-    recorded = keyboard.record(until='esc')
-    # Then replay back at three times the speed.
-    keyboard.play(recorded, speed_factor=3)
+	# Record events until 'esc' is pressed.
+	recorded = keyboard.record(until='esc')
+	keyboard.play(recorded, speed_factor=3)
 
-    # Type @@ then press space to replace with abbreviation.
-    keyboard.add_abbreviation('@@', 'my.long.email@example.com')
-    input()
+	keyboard.add_abbreviation('@@', 'my.long.email@example.com')
+	input()
