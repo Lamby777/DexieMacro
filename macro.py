66ed18b4eed8fbc2983ddd3083ce40b4c26db8b3
Lamby777 <sheepareawesome777@gmail.com>
2019-12-09 21:04:18 -0500

    ui and terminal looks changes

--- macro.py
+++ macro.py
@@ -3,7 +3,8 @@ from termcolor import colored
 import keyboard, turtle as t, tkinter as tk
 
 print(colored("Welcome to DxMacro, a FOSS macro customizer written in Python.", "red"))
-print("")
+print("Your settings have been loaded, press enter to change them
+")
 
 #keyboard.press_and_release('shift+s, space')
 #keyboard.write('The quick brown fox jumps over the lazy dog.')
@@ -16,7 +17,7 @@ print("")
 
 input()
 	
-
+keyboard.remove_abbreviation
 def write_slogan():
 	print("Tkinter is easy to use!")
 
@@ -24,14 +25,18 @@ root = tk.Tk()
 frame = tk.Frame(root)
 frame.pack()
 
+text = tk.Text(
+	text="DexieMacro Options Page"
+)
+
 button = tk.Button(frame, 
 	text="QUIT", 
 	fg="red",
 	command=quit)
-button.pack(side=tk.LEFT)
+button.pack(side=tk.RIGHT)
 slogan = tk.Button(frame,
-text="Hello",
-command=write_slogan)
+	text="Hello",
+	command=write_slogan)
 slogan.pack(side=tk.LEFT)
 
 root.mainloop()
