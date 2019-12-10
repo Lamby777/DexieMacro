d7ee36c6a37fb63c0b6f3fffa701fd7555af5f8f
Lamby777 <sheepareawesome777@gmail.com>
2019-12-09 21:05:31 -0500

    bruh moment removed

--- macro.py
+++ macro.py
@@ -3,8 +3,7 @@ from termcolor import colored
 import keyboard, turtle as t, tkinter as tk
 
 print(colored("Welcome to DxMacro, a FOSS macro customizer written in Python.", "red"))
-print("Your settings have been loaded, press enter to change them
-")
+print("Your settings have been loaded, press enter to change them")
 
 #keyboard.press_and_release('shift+s, space')
 #keyboard.write('The quick brown fox jumps over the lazy dog.')
