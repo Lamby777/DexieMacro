88628ba98d88a45ded5a0beabb4ca01b36b2f6a5
Lamby777 <sheepareawesome777@gmail.com>
2019-12-10 17:07:46 -0500

    record macro improvements and added code

--- macro.py
+++ macro.py
@@ -9,13 +9,20 @@ def printHelp():
 	) for x in cmds]
 
 def recordMacro():
-    print("Type starting key. (You can always delete macros)")
-    keyboard.read_key()
+	print("Type starting key combo. (ex. ctrl+shift+x or alt+q)")
+	print("Macros can always be deleted, so if you mess up, use command " + colored("del", "red") + "!")
+	mkey = input()
+	print("Great! Now, press the keys that the macro should automate.")
+	print("End the list with Ctrl + Esc")
+	print("When you are ready to record them, press enter to start.")
+	recorded = keyboard.record(until='esc')
+	
+	#keyboard.play(recorded, speed_factor=3)
 
 cmds = {
 	"help": {"fn": printHelp, "desc": "Shows help for commands"},
 	"exit": {"fn": quit, "desc": "Quits the CLI, stops all macros"},
-    "new": {"fn": recordMacro, "desc": "Adds a new macro"}
+	"new": {"fn": recordMacro, "desc": "Adds a new macro"}
 }
 
 print(colored("Welcome to DxMacro, a FOSS CLI Macro tool written in Python3.", "red"))
@@ -32,9 +39,8 @@ while True:
 
 #keyboard.press_and_release('shift+s, space')
 #keyboard.write('The quick brown fox jumps over the lazy dog.')
-#keyboard.add_hotkey('ctrl+shift+a', print, args=('triggered', 'hotkey'))
 #keyboard.add_hotkey('page up', lambda: keyboard.write('foobar'))
+#keyboard.add_hotkey('esc', print, args=("stuff"))
 #keyboard.wait('esc')
-#recorded = keyboard.record(until='esc')
 #keyboard.play(recorded, speed_factor=3)
 #keyboard.add_abbreviation('@@', 'my.long.email@example.com')
