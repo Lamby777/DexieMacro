a1115bb73b6495b79718e5270753766cff4fb611
Lamby777 <sheepareawesome777@gmail.com>
2019-12-09 22:47:23 -0500

    CLI help cmd and exit cmd

--- macro.py
+++ macro.py
@@ -3,8 +3,14 @@ from termcolor import colored
 import keyboard
 ver = "d7"
 
+def printHelp():
+	return [print(
+		x + ": \n\t" + cmds[x]["desc"] + "\n"
+	) for x in cmds]
+
 cmds = {
-	"exit": "haha"
+	"help": {"fn": printHelp, "desc": "Shows help for commands"},
+	"exit": {"fn": quit, "desc": "Quits the CLI, stops all macros"},
 }
 
 print(colored("Welcome to DxMacro, a FOSS CLI Macro tool written in Python3.", "red"))
@@ -15,7 +21,9 @@ while True:
 	print("> ", end="")
 	cmd = input()
 	if cmd in cmds.keys():
-		quit()
+		cmds[cmd]["fn"]()
+	else:
+		print(colored("Command does not exist, try \"help\"", "red"))
 #keyboard.press_and_release('shift+s, space')
 #keyboard.write('The quick brown fox jumps over the lazy dog.')
 #keyboard.add_hotkey('ctrl+shift+a', print, args=('triggered', 'hotkey'))
