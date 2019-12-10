15fe494ae17daa796ee41967b57723783a8e2529
Lamby777 <sheepareawesome777@gmail.com>
2019-12-09 22:13:34 -0500

    removed all of tk

--- macro.py
+++ macro.py
@@ -14,28 +14,3 @@ print("DxMacro is running version " + ver + "\n")
 #recorded = keyboard.record(until='esc')
 #keyboard.play(recorded, speed_factor=3)
 #keyboard.add_abbreviation('@@', 'my.long.email@example.com')
-
-class App(tk.Tk): 
-    def __init__(self, title="Untitled", icon="python.ico", res="200x200"): 
-        super().__init__()
-        self.title(title)
-        self.iconbitmap(icon)
-        self.geometry(res)
-
-def write_slogan():
-	print("Tkinter is HARD to use!")
-
-root = tk.Tk()
-frame = tk.Frame(root)
-frame.pack()
-
-title = tk.Text(frame)
-title.pack()
-title.insert(tk.END, "Just a text Widget\nin two lines\n")
-
-slogan = tk.Button(frame,
-	text="Hello",
-	command=write_slogan)
-slogan.pack(side=tk.LEFT)
-
-root.mainloop()
